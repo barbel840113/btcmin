bae1000116cf841b65969174592f81cd7e00177f
Michal Sivak <sivakmichal@gmail.com>
2017-11-27 23:17:43 +1100

    mergE

--- dist/main.bundle.js
+++ dist/main.bundle.js
@@ -4786,11 +4786,26 @@ var UploadfileComponent = /** @class */ (function () {
 
 "use strict";
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return environment; });
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+var protocol = window.location.protocol;
+var httpProtocol = '';
+var host = null;
+if (protocol == 'http:') {
+    httpProtocol = 'http://';
+    host = window.location.hostname + ':5000/';
+}
+if (protocol == 'https:') {
+    httpProtocol = 'https://';
+    host = window.location.host + '/';
+}
 var environment = {
     production: false,
-    envName: "staging",
-    identityUrl: 'http://appbuddiesidentity.azurewebsites.net',
-    appresource1Url: 'http://appbuddiesidentity.azurewebsites.net'
+    envName: 'dev',
+    identityUrl: httpProtocol + window.location.hostname + ':5000/',
+    appresource1Url: httpProtocol + window.location.hostname + ':5001/',
 };
 
 
