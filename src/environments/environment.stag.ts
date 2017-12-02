b923e31957e7e7db3af087c4eb8b0ffe1632eb2f
Michal Sivak <sivakmichal@gmail.com>
2017-12-02 19:53:05 +1100

    Fix environment.stag.ts

--- src/environments/environment.stag.ts
+++ src/environments/environment.stag.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     envName: "staging",
-    identityUrl: 'http://appbuddiesidentity.azurewebsites.net',
-    appresource1Url:'http://appbuddiesidentity.azurewebsites.net'
+    identityUrl: 'http://appbuddiesidentity.azurewebsites.net/',
+    appresource1Url:'http://appbuddiesidentity.azurewebsites.net/'
   };
